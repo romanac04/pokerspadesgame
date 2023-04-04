8510645b9726c9fb2aeb0d0482d9ad820444f456
unknown <romanac04@gmail.com>
2023-04-04 11:21:03 -0400

    placed all functions under one class

--- pokerstest.py
+++ pokerstest.py
@@ -7,20 +7,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(check_straight('S3', 'SQ', 'SK'), 0)
         self.assertEqual(check_straight('SA', 'S2', 'S3'), 0)
 
-class TestCheck3OfAKind(unittest.TestCase):
     def test_three_of_a_kind(self):
         self.assertEqual(check_3ofa_kind('S9', 'S9', 'S9'), 9)
         self.assertEqual(check_3ofa_kind('SA', 'SA', 'SA'), 14)
         self.assertEqual(check_3ofa_kind('S2', 'S2', 'S2'), 2)
         self.assertEqual(check_3ofa_kind('SJ', 'SQ', 'SJ'), 0)
 
-class MyTestCase(unittest.TestCase):
     def test_check_royal_flush(self):
         self.assertEqual(check_royal_flush('SA', 'SK', 'SQ'), 14)
         self.assertEqual(check_royal_flush('S10', 'SJ', 'SQ'), 0)
         self.assertEqual(check_royal_flush('SJ', 'SQ', 'SK'), 0)
 
-class MyTestCase(unittest.TestCase):
     def test_play_cards(self):
         self.assertEqual(play_cards('S2', 'S3', 'S4', 'S5', 'S6', 'S7'), 1)
         self.assertEqual(play_cards('S2', 'S3', 'S4', 'S4', 'S4', 'S5'), -1)
